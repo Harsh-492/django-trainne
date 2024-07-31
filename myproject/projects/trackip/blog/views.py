e8e8686f590aaa0b27cf4c1b9acc031cda424ae6
hiten <hiten@mail.swansoftweb.net>
2024-07-31 17:50:49 +0530

    permission

--- myproject/projects/trackip/blog/views.py
+++ myproject/projects/trackip/blog/views.py
@@ -1,118 +1,93 @@
-from django.shortcuts import render,HttpResponseRedirect
-from .forms import SignupForm,LoginForm,PostForm
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView, ListView, DetailView, UpdateView, DeleteView,View
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import redirect
 from .models import Post
+from .forms import SignupForm, LoginForm, PostForm
 from django.contrib.auth.models import Group
-# Create your views here.
-def home(request):
-    post = Post.objects.all()
-    print(post)
-    return render(request,'blog/home.html',{'post':post})
 
+class HomeView(TemplateView):
+    template_name = 'blog/home.html'
 
-def about(request):
-    return render(request,'blog/about.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = Post.objects.all()
+        return context
 
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
 
-def contact(request):
-    return render(request,'blog/contact.html')
+class ContactView(TemplateView):
+    template_name = 'blog/contact.html'
 
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'blog/dashboard.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = Post.objects.all()
+        user = self.request.user
+        context['full_name'] = user.get_full_name()
+        context['groups'] = user.groups.all()
+        context['ip'] = self.request.session.get('ip', 0)
+        return context
 
-def dashboard(request):
-    if request.user.is_authenticated:
-        post = Post.objects.all()
-        user = request.user
-        full_name = user.get_full_name()
-        gps = user.groups.all()
-        print(post)
-        print('user : ',user)
-        print("Full Name : ",full_name)
-        print("GPS : ",gps)
-        ip = request.session.get('ip',0)
-        return render(request,'blog/dashboard.html',{'post':post,'full_name':full_name,'groups':gps,'ip':ip})
-    else:
-        return HttpResponseRedirect('/login/')
+class SignupView(FormView):
+    template_name = 'blog/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('login')  # Redirect to login page after successful signup
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Congratulations! You have become an Author !!!')
+        user = form.save()
+        group = Group.objects.get(name='Author')
+        user.groups.add(group)
+        return super().form_valid(form)
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            messages.success(request,'Congratulations! You  have become a Author !!!')
-            user = form.save()
-            group = Group.objects.get(name='Author')
-            user.groups.add(group)
-        return render(request,'blog/signup.html',{'form':form})
-    else:
-        form = SignupForm()
-        return render(request,'blog/signup.html',{'form':form})
+class UserLoginView(FormView):
+    template_name = 'blog/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('dashboard')
 
+    def form_valid(self, form):
+        un = form.cleaned_data['username']
+        pw = form.cleaned_data['password']
+        user = authenticate(username=un, password=pw)
+        if user is not None:
+            login(self.request, user)
+            messages.success(self.request, "Logged in successfully !!!")
+        return super().form_valid(form)
 
-def user_login(request):
-    if not request.user.is_authenticated:
-        if request.method == 'POST':
-            form = LoginForm(request=request,data=request.POST)
-            if form.is_valid():
-                un = form.cleaned_data['username']
-                pw = form.cleaned_data['password']
-                user = authenticate(username=un, password=pw)
-                if user is not None:
-                    login(request,user)
-                    messages.success(request,"Logged in successfully !!!")
-                    return HttpResponseRedirect('/dashboard/')
-        else:
-            form = LoginForm()
-        return render(request,'blog/login.html',{'form':form})
-    else:
-        return HttpResponseRedirect('/dashboard/')
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            return reverse_lazy('dashboard')
+        return super().get_success_url()
 
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect('/user_login/')
+class UserLogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('/user_login/')
 
+class AddPostView(LoginRequiredMixin, FormView):
+    template_name = 'blog/addpost.html'
+    form_class = PostForm
+    success_url = reverse_lazy('dashboard')
 
-def add_post(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = PostForm(request.POST)
-            if form.is_valid():
-                title = form.cleaned_data['title']
-                description = form.cleaned_data['description']
-                post = Post(title=title, description=description)
-                post.save()
-                form = PostForm()
-        else:
-            form = PostForm()
-        return render(request, 'blog/addpost.html',{'form':form})
-        
-    else:
-        return HttpResponseRedirect('/login/')
-    
+    def form_valid(self, form):
+        title = form.cleaned_data['title']
+        description = form.cleaned_data['description']
+        Post.objects.create(title=title, description=description)
+        return super().form_valid(form)
 
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/updatepost.html'
+    success_url = reverse_lazy('dashboard')
 
-def update_post(request,pk):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            pi = Post.objects.get(pk=pk)
-            form = PostForm(request.POST,instance=pi)
-            if form.is_valid():
-                form.save()
-                return HttpResponseRedirect('/dashboard/')
-        else:
-            pi = Post.objects.get(pk=pk)
-            form = PostForm(instance=pi)
-        return render(request, 'blog/updatepost.html',{'form':form})
-    else:
-        return HttpResponseRedirect('/login/')
-    
-
-def delete_post(request,id):
-    if request.user.is_authenticated:
-           if request.method == 'POST':
-                pi = Post.objects.get(pk=id)
-                pi.delete()
-                return HttpResponseRedirect('/dashboard/')
-    else:
-        return HttpResponseRedirect('/login/')
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/deletepost.html'
+    success_url = reverse_lazy('dashboard')
